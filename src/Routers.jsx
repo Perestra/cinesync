ab36efe791a167d65ec15bb5ea6c9de1b8beaa41
Perestra <danilo.ps.1996@gmail.com>
2023-11-07 07:16:15 -0300

    Removed the Header component in Routers

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Initial from 'src/pages/Initial/Initial'
-import Header from 'src/components/Header/Header'
 
 const Routers = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route index element={ <Initial /> }/>
       </Routes>
