93b2c2653bab039a784308d44264637569ad3009
Perestra <danilo.ps.1996@gmail.com>
2023-11-08 19:31:45 -0300

    Added the Login page in Route

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Initial from 'src/pages/Initial/Initial'
 import Footer from 'src/components/Footer/Footer'
+import Login from 'src/pages/Login/Login'
 
 const Routers = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={ <Initial /> }/>
+        <Route path='/login' element={ <Login /> } />
       </Routes>
       <Footer />
     </BrowserRouter>
