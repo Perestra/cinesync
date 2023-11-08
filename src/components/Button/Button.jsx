17a55d1e1ea22451a38ca14350842a0e25c15932
Perestra <danilo.ps.1996@gmail.com>
2023-11-08 19:32:27 -0300

    Few changes in button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import styles from './Button.module.scss'
 
-const Button = ({ title }) => {
+const Button = ({ type, className, onClick, title }) => {
   return (
-    <button type="button" className={ styles.button }>
-        <p className={ styles.button__text }>{ title }</p>
+    <button type={ type } className={ className } onClick={ onClick }>
+        <p>{ title }</p>
     </button>
   )
 }
