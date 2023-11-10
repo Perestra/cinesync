b16aa027ecc194afaed7e292e19647f8cc662cbb
Perestra <danilo.ps.1996@gmail.com>
2023-11-10 19:00:17 -0300

    Added a new content style variable

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,12 @@ import styles from './Header.module.scss'
 import logoWhite from 'src/assets/svg/logoWhite.svg';
 import NavList from 'src/components/NavList/NavList';
 
-const Header = () => {
+const Header = ({ display, content }) => {
   return (
     <header className={ styles.header }>
-      <div className={ styles.header__container }>
+      <div className={ `${styles.header__container} ${styles[content]} ` }>
         <img className={ styles.header__logo } src={ logoWhite } alt='Logo da CineSync na cor branca' />
-        <div className={ styles.header__nav }>
+        <div className={ `${styles.header__nav} ${styles[display]}` }>
           <NavList />
         </div>
       </div>
