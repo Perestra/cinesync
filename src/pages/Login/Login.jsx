cf02dec8a9fd7fa8284d43c445aaee55492e898d
Perestra <danilo.ps.1996@gmail.com>
2023-11-10 19:04:50 -0300

    Added useForm to validade forms

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,16 +5,26 @@ import Header from 'src/components/Header/Header'
 import Subtitle from 'src/components/Subtitle/Subtitle'
 import Button from 'src/components/Button/Button'
 import Text from 'src/components/Text/Text'
-import { NavLink } from 'react-router-dom'
+
+import { useForm } from 'react-hook-form'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const Login = () => {
+
+    const navigate = useNavigate()
+    const { register, handleSubmit, formState:{ errors }, } = useForm()
+
+    const onSubmit = data => {
+        console.log(data)
+    } 
+
   return (
     <div className={ styles.login }>
-        <Header />
+        <Header display='nonVisible' content='center'/>
         <main className={ styles.login__main }>
             <section className={ styles.login__section }>
-                <Subtitle text='Login'/>   
-                <form className={ styles.login__form }>
+                <Subtitle text='Login' color='orange'/>   
+                <form className={ styles.login__form } onSubmit={handleSubmit(onSubmit)}>
                     <div className={ styles.login__field}>
                         <label htmlFor="username"className={ styles.login__label } >Usuário</label>
                         <input 
@@ -22,8 +32,10 @@ const Login = () => {
                             name="username" 
                             id="username"
                             placeholder='Digite seu usuário'
-                            className={ styles.login__input }
+                            className={ `${styles.login__input} ${errors?.username && styles.login__inputError}` }
+                            {...register("username", { required: true })}
                         />    
+                        { errors?.username?.type === 'required' && <Text className='error' text='Este campo é obrigatório!' /> }
                     </div>
                     <div className={ styles.login__field }>
                         <div className={ styles.login__password }>
@@ -35,14 +47,16 @@ const Login = () => {
                             name="password" 
                             id="password" 
                             placeholder='Digite sua senha'
-                            className={ styles.login__input }
-                        />    
+                            className={ `${styles.login__input} ${errors?.username && styles.login__inputError}` }
+                            {...register("password", { required: true })}
+                        /> 
+                        { errors?.password?.type === 'required' && <Text className='error' text='Este campo é obrigatório!' /> }   
                     </div>
-                    <Button className={ styles.login__btnSubmit } type='submit' title='Entrar' onClick />
+                    <Button btnClassName={ styles.login__btnSubmit } txtClassName='white' type='submit' title='Entrar'/>
                 </form>     
                 <div className={ styles.login__createAccount }>
-                    <Text text='É novo por aqui?'/>
-                    <Button className={ styles.login__btnCreateAccount } type='submit' title='Criar conta'/>
+                    <Text className='gray' text='É novo por aqui?'/>
+                    <Button btnClassName={ styles.login__btnCreateAccount } txtClassName='black' title='Criar conta' onClick={ () => navigate('/criarconta') }/>
                 </div>
             </section> 
         </main>
