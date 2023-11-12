2851f2b2084035f700cc5b1b23731cfc5688ad42
Perestra <danilo.ps.1996@gmail.com>
2023-11-12 20:16:52 -0300

    Added the context provider component

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { AccountProvider } from 'src/context/AccountContext'
 
 import Initial from 'src/pages/Initial/Initial'
 import Login from 'src/pages/Login/Login'
@@ -10,11 +11,13 @@ import Footer from 'src/components/Footer/Footer'
 const Routers = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route index element={ <Initial /> }/>
-        <Route path='/login' element={ <Login /> } />
-        <Route path='/criarconta' element={ <CreateAccount /> } />
-      </Routes>
+      <AccountProvider> 
+        <Routes>
+          <Route index element={ <Initial /> }/>
+          <Route path='/login' element={ <Login /> } />
+          <Route path='/criarconta' element={ <CreateAccount /> } />
+        </Routes>
+      </AccountProvider>
       <Footer />
     </BrowserRouter>
   )
