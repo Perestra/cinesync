2df6cb129d391644a048a33736e8bb9389701edd
Perestra <danilo.ps.1996@gmail.com>
2023-11-14 19:58:54 -0300

    Created the ForgotPassword page

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -6,6 +6,7 @@ import { AccountProvider } from 'src/contexts/AccountContext'
 import Initial from 'src/pages/Initial/Initial'
 import Login from 'src/pages/Login/Login'
 import CreateAccount from 'src/pages/CreateAccount/CreateAccount'
+import ForgotPassword from 'src/pages/ForgotPassword/ForgotPassword'
 import Footer from 'src/components/Footer/Footer'
 
 const Routers = () => {
@@ -16,6 +17,7 @@ const Routers = () => {
           <Route index element={ <Initial /> }/>
           <Route path='/login' element={ <Login /> } />
           <Route path='/criarconta' element={ <CreateAccount /> } />
+          <Route path='/esqueciminhasenha' element={ <ForgotPassword /> } />
         </Routes>
       </AccountProvider>
       <Footer />
---
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import styles from './ForgotPassword.module.scss'
+
+import InputForm from 'src/components/InputForm/InputForm'
+import Subtitle from 'src/components/Subtitle/Subtitle'
+import Header from 'src/components/Header/Header'
+import Button from 'src/components/Button/Button'
+import Text from 'src/components/Text/Text'
+import { useAccountContext } from 'src/hooks/useAccountContext'
+import { TbAlertCircleFilled } from 'react-icons/tb'
+import { useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+
+const ForgotPassword = () => {
+
+    const { userPassword, isValidEmail, showPassword } = useAccountContext()
+    const { register, handleSubmit, formState:{ errors }, } = useForm()
+    const navigate = useNavigate()
+
+  return (
+    <div className={ styles.forgotPassword }>
+        <Header display='nonVisible' content='center'/>
+        <main className={ styles.forgotPassword__main }>
+            <Subtitle text='Esqueci minha senha' color='orange' />
+            <section className={ styles.forgotPassword__inputEmail }>
+                <form className={ styles.forgotPassword__form } onSubmit={ handleSubmit(showPassword) } >
+                    <InputForm 
+                        name='email'
+                        label='E-mail'
+                        type='email'
+                        placeholder='Digite seu e-mail'
+                        typeClassName='form__input'
+                        styleClassName={errors?.email && "form__inputError"}
+                        register={register}
+                        required
+                        validate={value => isValidEmail(value) }
+                        icon={errors?.email && <TbAlertCircleFilled className={ styles.forgotPassword__iconError } />}
+                        errorText= { errors?.email?.type === 'required'? <Text className='error' text='Este campo é obrigatório!' /> : errors?.email?.type === 'validate'? <Text className='error' text='Não existe uma conta com esse e-mail!' /> : "" }
+                    />
+                    <Button btnClassName={ styles.forgotPassword__btnSubmit } txtClassName='white' type='submit' title='Continuar'/>    
+                </form>
+                { console.log(errors) }
+            </section>
+            <section className={ styles.forgotPassword__passwordData }>
+                <Text className='white' text='Senha:'/>
+                <div className={ styles.forgotPassword__password }>
+                    <Text className='black' text={ userPassword }/>
+                </div>
+            </section>
+            <Button btnClassName={ styles.forgotPassword__btnLogin } txtClassName='black' title='Ir para o login' onClick={ () => navigate('/login') }/>
+        </main>
+    </div>
+  )
+}
+
+export default ForgotPassword
