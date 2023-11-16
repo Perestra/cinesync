ddfb8170980039629bc163cc73f1e336c9b47eb6
Perestra <danilo.ps.1996@gmail.com>
2023-11-16 07:43:00 -0300

    Created the tmdbApi axios instance

---
+++ src/api/tmdbApi.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const tmdb = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  params: {
+    api_key: '875eb7acef457f17217538c2f06f2cec',
+    language: 'pt-BR'  
+  }
+});
+
+  export default tmdb
