fd50a062026c1daab4f02f49ba42ae506ea508c8
Perestra <danilo.ps.1996@gmail.com>
2023-11-18 21:46:15 -0300

    Added functions to get some API's value

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,35 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Home.module.scss'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
 import Header from 'src/components/Header/Header'
-import Text from 'src/components/Text/Text'
-import Button from 'src/components/Button/Button'
 import BlockBusters from 'src/components/BlockBusters/BlockBusters';
-
-import { IoPlayCircleOutline } from "react-icons/io5";
-import { IoMdInformationCircleOutline } from "react-icons/io";
+import HighlightFilm from 'src/components/HighlightFilm/HighlightFilm';
 
 const Home = () => {
+
+  const infoHighlight = (id, title, overview, backdrop_path, media_type) => [{
+    id,
+    title,
+    overview,
+    backdrop_path,
+    media_type
+  }]
+
+  const [ mainBlockBuster, setMainBlockBuster ] = useState(infoHighlight(
+    872585, 
+    'Oppenheimer',
+    'A história do físico americano J. Robert Oppenheimer, seu papel no Projeto Manhattan e no desenvolvimento da bomba atômica durante a Segunda Guerra Mundial, e o quanto isso mudaria a história do mundo para sempre.',
+    "/fm6KqXpk3M2HVveHwCrBSSBaO0V.jpg",
+    'movie'
+  ))
+  
+  const getBlockBusterData = (id, title, overview, backdrop_path, media_type) => {
+    setMainBlockBuster(infoHighlight(id, title, overview, backdrop_path, media_type))
+  }
 
   return (
     <div className={ styles.home }>
       <Header content='visible' />
-      <section className={ styles.home__sectionData }>
-        <div className={ styles.home__filmData }>
-          <img src="" alt="Logo do filme" />
-          <Text className='white' text='Miles Morales retorna para o próximo capítulo da saga do Aranhaverso, uma aventura épica que transportará o Homem-Aranha em tempo integral e amigável do bairro do Brooklyn através do Multiverso para unir forças com Gwen Stacy.' />
-          <div className={ styles.home__buttons }>
-              <Button btnClassName={ styles.home__btnPlay } txtClassName='white' title='Reproduzir' type='button' icon={<IoPlayCircleOutline className={ styles.home__iconPlay } />} />
-              <Button btnClassName={ styles.home__btnKnowMore } txtClassName='black' title='Saiba mais' type='button' icon={<IoMdInformationCircleOutline className={ styles.home__iconKnowMore } />} />
-          </div>   
-        </div>
-      </section>
+      <HighlightFilm mainBlockBuster={ mainBlockBuster } />
       <main className={ styles.home__main }>
-        <BlockBusters posterTitle='Populares da semana' url='/trending/all/week' />
-        <BlockBusters posterTitle='Filmes populares' url='/trending/movie/week' />
-        <BlockBusters posterTitle='Séries populares' url='/trending/tv/week' />
+        <BlockBusters posterTitle='Populares da semana' url='/trending/all/week' getBlockBusterData={ getBlockBusterData } />
+        <BlockBusters posterTitle='Filmes populares' url='/trending/movie/week' getBlockBusterData={ getBlockBusterData } />
+        <BlockBusters posterTitle='Séries populares' url='/trending/tv/week' getBlockBusterData={ getBlockBusterData } />
       </main>
     </div>
   )
