f5374ea7ec1a6566d10c973ff2abc00db68a592e
Perestra <danilo.ps.1996@gmail.com>
2023-11-18 21:44:47 -0300

    Added onClick property

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import styles from './Card.module.scss'
 
-const Card = ({ src, alt }) => {
+const Card = ({ src, alt, onClick }) => {
   return (
-    <div className={ styles.card }>
+    <div onClick={ onClick } className={ styles.card }>
       <img className={ styles.card__image } src={ src } alt={ alt } />
     </div>
   )
