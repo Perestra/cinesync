9f0995fbbb368df44a33982401da3bb02140e493
Perestra <danilo.ps.1996@gmail.com>
2023-11-26 20:29:09 -0300

    Created the custom hook for AuthContext

---
+++ src/hooks/useAuthContext.jsx
@@ -0,0 +1,43 @@
+import { useContext } from "react"
+import { useNavigate } from "react-router-dom"
+
+import { AccountContext } from "src/contexts/AccountContext"
+import { AuthContext } from "src/contexts/AuthContext"
+import { v4 as uuid } from "uuid"
+
+
+export function useAuthContext() {
+
+    const { accounts } = useContext(AccountContext)
+    const { authUser, setAuthUser } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    const generateAuth = data => {
+        const infoAccount = {
+            id: data.id,
+            fullname: data.fullname,
+            username: data.username,
+            email: data.email,
+            password: data.password,
+            token: uuid()
+        }
+        setAuthUser(infoAccount)
+    } 
+  
+    const submitSignIn = ( data ) => {
+        const findedAccount = accounts.find(account => data.username === account.username && data.password === account.password)
+        if(findedAccount) {
+            generateAuth(findedAccount)
+            navigate('/inicio')
+        } 
+    }
+
+    const isLoginSuccess = () => {
+        const findUserId = accounts.find( account => account.id === authUser?.id )
+        if(findUserId) return true 
+        return false
+    }
+
+
+    return { isLoginSuccess, submitSignIn }
+}
