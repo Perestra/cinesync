90e84bf1b43ba0baa55ec1b30745e07e6ca9cfe6
Perestra <danilo.ps.1996@gmail.com>
2023-12-01 13:04:22 -0300

    Added a new function to modify vote

--- src/helper/modifyDatas.js
+++ src/helper/modifyDatas.js
@@ -2,18 +2,24 @@
 const runTime = time => {
     const hour = Math.floor(time/60)
     const min = time%60
-    return `${hour}h ${min.toString().padStart(2,'0')}m`
+    return `${hour}h ${min.toString().padStart(2,'0')}min`
 }
 
 const tvDate = (firstDate, lastDate) => {
-    const firstYear = firstDate.split('-')[0]
-    const lastYear = lastDate.split('-')[0]
+    const firstYear = firstDate?.split('-')[0]
+    const lastYear = lastDate?.split('-')[0]
     return `${firstYear} - ${lastYear}`
 }
 
 const movieDate = date => {
-    return year = date.split('-')[0]
+    const year = date?.split('-')[0]
+    return year
 }
 
-export { runTime, tvDate, movieDate }
+const rating = data => {
+    const round = data?.toFixed(1)
+    return round
+}
+
+export { runTime, tvDate, movieDate, rating }
 
