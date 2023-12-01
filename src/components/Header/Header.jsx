9dd09cc1d62578879c94054fd70bce6ff577ed12
Perestra <danilo.ps.1996@gmail.com>
2023-12-01 13:05:19 -0300

    Added and styled the user field in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import styles from './Header.module.scss'
 
-import logoWhite from 'src/assets/svg/logoWhite.svg';
 import NavList from 'src/components/NavList/NavList';
+import { IoIosArrowDown } from "react-icons/io";
 
-const Header = ({ display, content }) => {
+const Header = ({ display, content, username }) => {
   return (
     <header className={ styles.header }>
       <div className={ `${styles.header__container} ${styles[content]} ` }>
-        <img className={ styles.header__logo } src={ logoWhite } alt='Logo da CineSync na cor branca' />
+        <span>cineSync</span>
         <div className={ `${styles.header__nav} ${styles[display]}` }>
           <NavList />
         </div>
+        <div className={ styles.header__user }>
+          <span>{ username }</span>
+          <IoIosArrowDown className={ `${styles.header__userIcon} ${styles[display]}` } />
+        </div>
       </div>
     </header>
-  )
+  ) 
 }
 
 export default Header
