ac05f4a86a11c54e1a3d1f5304a43d799e29c3c2
Perestra <danilo.ps.1996@gmail.com>
2023-12-06 19:42:56 -0300

    Changed the function name to getTrailerKey

--- src/helper/getTrailerKey.js
+++ src/helper/getTrailerKey.js
@@ -1,12 +1,12 @@
 import tmdb from "src/api/tmdbApi"
+
 import { useAxios } from "src/hooks/useAxios"
 
-const getTrailer = (media, id) => {
+const getTrailerKey = (media, id) => {
     const { results, isLoading, error } = useAxios( tmdb, 'get', `/${media}/${id}/videos`)
     const trailer = results.filter( video => video.type.toLowerCase() === 'trailer')
-
-    const trailerBaseURL = 'http://www.youtube.com/embed/'
-    return `${trailerBaseURL}${trailer[0]?.key}`
+    return trailer[0]?.key
 }
 
-export default getTrailer
+export { getTrailerKey }
+
