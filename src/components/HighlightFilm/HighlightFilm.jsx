b2963121a2c6566de9c6e2e6a6c86524e124176b
Perestra <danilo.ps.1996@gmail.com>
2023-12-06 19:42:28 -0300

    Added a API request in Highlight component

--- src/components/HighlightFilm/HighlightFilm.jsx
+++ src/components/HighlightFilm/HighlightFilm.jsx
@@ -4,22 +4,31 @@ import styles from './HighlightFilm.module.scss'
 import Title from 'src/components/Title/Title'
 import Text from 'src/components/Text/Text'
 import Button from 'src/components/Button/Button'
-import getTrailer from 'src/helper/trailerURL'
 import imdb from 'src/assets/images/imdb.png'
+import tmdb from 'src/api/tmdbApi'
 
 import { IoPlayCircleOutline } from "react-icons/io5";
 import { IoMdInformationCircleOutline } from "react-icons/io";
 import { movieDate, tvDate, runTime, rating } from 'src/helper/modifyDatas';
+import { useAxios } from 'src/hooks/useAxios'
 
-const HighlightFilm = ({ data, filmData }) => {
+const HighlightFilm = ({ filmData }) => {
+
+    const { data, isLoading, error } = useAxios(tmdb, 'get', `/${filmData.media}/${filmData.id}`)
+
+    const { results } = useAxios( tmdb, 'get', `/${filmData.media}/${filmData.id}/videos`)
+    const trailer = results.filter( video => video.type.toLowerCase() === 'trailer')
+
+    const trailerBaseURL = 'http://www.youtube.com/embed/'
+
     return (
     <section className={ styles.HighlightFilm }>
         <div className={ styles.HighlightFilm__dataContainer }>
-            <div className={ styles.HighlightFilm__data }>
+            { data && <div className={ styles.HighlightFilm__data }>
                 <div className={ styles.HighlightFilm__date }>
-                    <Text className='white' text={ data.release_date? movieDate(data.release_date): tvDate(data.first_air_date, data.last_air_date) } />
-                    <Text className='white' text={ runTime(data.runtime) } />
-                </div>
+                    { (data.release_date || data.first_air_date) && <Text className='white' text={ data.release_date? movieDate(data.release_date): tvDate(data.first_air_date, data.last_air_date) } />}
+                    { data.runtime && <Text className='white' text={ runTime(data.runtime) } /> }
+                </div> 
                 <Title 
                     key={data.title? data.title: data.name}
                     animation='tilt-in-left-1'
@@ -28,21 +37,12 @@ const HighlightFilm = ({ data, filmData }) => {
                 <div className={ styles.HighlightFilm__genres }>
                     { data.genres?.map( genre => <Text key={ genre.name } className='white' text={ genre.name } /> ) }
                 </div>
-                <div className={ styles.HighlightFilm__rating }>
+                { data.vote_average && <div className={ styles.HighlightFilm__rating }>
                     <img src={imdb} alt="Logo do IMDB" />
                     <Text className='white' text={rating(data.vote_average)} />
-                </div>
-            </div>
+                </div>}
+            </div> }
             <div className={ styles.HighlightFilm__buttons }>
-               <Button 
-                    btnClassName={ styles.HighlightFilm__btnPlay }
-                    txtClassName='white' 
-                    title='Assistir ao trailer' 
-                    type='button' 
-                    href={getTrailer(filmData.media, filmData.id)}
-                    target='_blank'
-                    icon={<IoPlayCircleOutline className={ styles.HighlightFilm__iconPlay } />} 
-                />
                 <Button 
                     btnClassName={ styles.HighlightFilm__btnKnowMore } 
                     txtClassName='black' 
@@ -50,6 +50,15 @@ const HighlightFilm = ({ data, filmData }) => {
                     type='button' 
                     icon={<IoMdInformationCircleOutline className={ styles.HighlightFilm__iconKnowMore } />} 
                 />
+                { trailer[0]?.key && <Button 
+                    btnClassName={ styles.HighlightFilm__btnPlay }
+                    txtClassName='white' 
+                    title='Assistir ao trailer' 
+                    type='button' 
+                    href={ `${trailerBaseURL}${trailer[0]?.key}` }
+                    target='_blank'
+                    icon={<IoPlayCircleOutline className={ styles.HighlightFilm__iconPlay } />} 
+                /> }
             </div> 
         </div>    
     </section>
