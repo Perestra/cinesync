df9b57c4b73451096d2be9eb93067480224ba564
Perestra <danilo.ps.1996@gmail.com>
2023-12-07 08:28:46 -0300

    Added the FilmDetails as a Movie's Child

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -13,6 +13,7 @@ import Footer from 'src/components/Footer/Footer'
 import Home from 'src/pages/Home/Home'
 import Movies from 'src/pages/Movies/Movies'
 import TvShow from 'src/pages/TvShow/TvShow'
+import FilmDetails from 'src/pages/FilmDetails/FilmDetails'
 
 const Routers = () => {
   return (
@@ -21,12 +22,18 @@ const Routers = () => {
         <AccountProvider> 
           <Routes>
             <Route index element={ <Initial /> }/>
-            <Route path='/login' element={ <Login /> } />
-            <Route path='/criarconta' element={ <CreateAccount /> } />
-            <Route path='/esqueciminhasenha' element={ <ForgotPassword /> } />
-            <Route path='/inicio' element={ <RequireAuth><Home /></RequireAuth> }/>  
-            <Route path='/filmes' element={ <RequireAuth><Movies /></RequireAuth> }/>  
-            <Route path='/series' element={ <RequireAuth><TvShow /></RequireAuth> }/>  
+            <Route path='login' element={ <Login /> } />
+            <Route path='criarconta' element={ <CreateAccount /> } />
+            <Route path='esqueciminhasenha' element={ <ForgotPassword /> } />
+            <Route path='inicio' element={ <RequireAuth><Home /></RequireAuth> }/>  
+            <Route path='filmes'>
+              <Route index element={ <RequireAuth><Movies /></RequireAuth> }/>  
+              <Route path=':id' element={ <RequireAuth><FilmDetails /></RequireAuth> }/>  
+            </Route>  
+            <Route path='series'>
+              <Route index element={ <RequireAuth><TvShow /></RequireAuth> }/>
+              {/* <Route path=':id' element={ <RequireAuth><FilmDetails /></RequireAuth> }/> */}
+            </Route>  
           </Routes>
         </AccountProvider>
         <Footer />
