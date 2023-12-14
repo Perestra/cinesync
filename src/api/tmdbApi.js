d4ff49b65f76ca7a5b5ffec76a1f6aa9c33d31b2
Perestra <danilo.ps.1996@gmail.com>
2023-12-14 09:39:09 -0300

    Added a new value in language parameter

--- src/api/tmdbApi.js
+++ src/api/tmdbApi.js
@@ -4,7 +4,7 @@ const tmdb = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
     api_key: '875eb7acef457f17217538c2f06f2cec',
-    language: 'pt-BR',
+    language: 'pt-BR,null',
     include_image_language: 'pt,null'
   }
 });
