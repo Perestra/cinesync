46386b1c8e02d82ab9568f8682dfc76d68d518ff
Perestra <danilo.ps.1996@gmail.com>
2023-12-21 19:40:56 -0300

    Added the userMenu component into Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Header.module.scss'
 
 import NavList from 'src/components/NavList/NavList';
+import UserMenu from 'src/components/UserMenu/UserMenu';
+
 import { IoIosArrowDown } from "react-icons/io";
 
 const Header = ({ display, content, username }) => {
+
+  const [ userMenu, setUserMenu ] = useState(false)
+
   return (
     <header className={ styles.header }>
-      <div className={ `${styles.header__container} ${styles[content]} ` }>
+      <div className={ `${styles.header__container}` }>
         <span>cineSync</span>
         <div className={ `${styles.header__nav} ${styles[display]}` }>
           <NavList />
         </div>
         <div className={ styles.header__user }>
           <span>{ username }</span>
-          <IoIosArrowDown className={ `${styles.header__userIcon} ${styles[display]}` } />
+          <IoIosArrowDown 
+            className={ `${styles.header__userIcon} ${styles[display]} ${styles[content]} ${ userMenu? styles.rotate: '' }` }
+            onClick={ () => setUserMenu(!userMenu) } 
+          />
         </div>
       </div>
+      <UserMenu 
+        menu={ userMenu? 'openMenu': ''} 
+        container ={userMenu? 'displayContent': ''} 
+      />
     </header>
   ) 
 }
