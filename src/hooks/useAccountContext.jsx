1af333a5acce4e614a725ddfcb7a47538cb39f43
Perestra <danilo.ps.1996@gmail.com>
2023-12-23 08:16:56 -0300

    Added a new  password validation function

--- src/hooks/useAccountContext.jsx
+++ src/hooks/useAccountContext.jsx
@@ -1,19 +1,24 @@
 import { useContext, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { AccountContext } from "src/contexts/AccountContext"
-import { AuthContext } from "src/contexts/AuthContext"
 import { v4 as uuid } from 'uuid'
+import { useAuthContext } from "./useAuthContext"
 
 export function useAccountContext() {
 
     const { accounts, setAccounts } = useContext(AccountContext)
-    const submitSignIn = useContext(AuthContext)
+    const { authUser, setAuthUser, submitSignIn } = useAuthContext()
     const [ userPassword, setUserPassword ] = useState('')
 
     const navigate = useNavigate()
 
     const isValidUsername = value => {
         const findedAccount = accounts.some(account => value === account.username)
+        return findedAccount
+    }
+
+    const isValidPassword = value => {
+        const findedAccount = accounts.some(account => value === authUser.password)
         return findedAccount
     }
 
@@ -43,5 +48,21 @@ export function useAccountContext() {
         navigate('/login')
     } 
 
-    return {accounts, userPassword, isValidUsername, isValidEmail, showPassword, createAccount, submitGenerateAccount, submitSignIn}
+    const ChangePassword = data => {
+        const userLogged = accounts.find( user => authUser.id === user.id )
+        const index = accounts.findIndex( user => authUser.id === user.id )
+
+        const newPassword = data.passwordChange
+
+        if(userLogged.password != data.passwordChange) {
+            const userAccounts = [...accounts]
+            userAccounts[index].password = newPassword
+
+            setAccounts( userAccounts )
+            setAuthUser()
+            navigate('/login')
+        } 
+    }
+
+    return {accounts, setAccounts, userPassword, isValidUsername, isValidPassword, isValidEmail, showPassword, createAccount, submitGenerateAccount, submitSignIn, ChangePassword}
 }
