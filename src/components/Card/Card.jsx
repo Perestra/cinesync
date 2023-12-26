98af7d5ad3ef67b2b47588909e0fa00d990a1203
Perestra <danilo.ps.1996@gmail.com>
2023-12-26 10:44:22 -0300

    Added the Start icon for add or remove from Watch List

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import styles from './Card.module.scss'
 
-const Card = ({ src, alt, onClick }) => {
+import { IoStar, IoStarOutline } from 'react-icons/io5'
+import { useAccountContext } from 'src/hooks/useAccountContext'
+import { useAuthContext } from 'src/hooks/useAuthContext'
+
+const Card = ({ id, type, name, date, src, alt, onClick }) => {
+
+  const { accounts, setAccounts } = useAccountContext()
+  const { authUser } = useAuthContext()
+
+  const userLogged = accounts.find( account => account.id === authUser.id )
+  const isInAccountWatchList = id => userLogged.watchList.find( movieId => movieId.id === id )
+
+  const addInWatchList = (id, name, type, date, src) => {
+    const movieData = {
+      id: id,
+      name: name,
+      media_type: type,
+      date: date,
+      poster_path: src
+    }
+    userLogged.watchList = [...userLogged.watchList, movieData]
+    setAccounts([...accounts])
+  }
+
+  const remFromWatchList = id => {
+    const remId = userLogged.watchList.filter( movieId => movieId.id !== id )
+    userLogged.watchList = [...remId]
+    setAccounts([...accounts])
+  }
+  
   return (
-    <div onClick={ onClick } className={ styles.card }>
-      <img className={ styles.card__image } src={ src } alt={ alt } />
+    <div className={ styles.card }>
+      { !isInAccountWatchList(id)?
+        <IoStarOutline className={ styles.card__icon } onClick={ () => addInWatchList(id, name, type, date, src) } />: 
+        <IoStar className={ styles.card__icon} onClick={ () => remFromWatchList(id) } />
+      }
+      <img className={ styles.card__image } src={ src } alt={ alt } onClick={ onClick }  />
     </div>
   )
 }
