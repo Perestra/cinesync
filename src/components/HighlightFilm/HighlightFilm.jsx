6426eb046be3c28c7339e6c5d2f155c9e4ca40ad
Perestra <danilo.ps.1996@gmail.com>
2023-12-26 10:46:01 -0300

    Added the getAPIData function to request's API

--- src/components/HighlightFilm/HighlightFilm.jsx
+++ src/components/HighlightFilm/HighlightFilm.jsx
@@ -5,19 +5,17 @@ import Title from 'src/components/Title/Title'
 import Text from 'src/components/Text/Text'
 import Button from 'src/components/Button/Button'
 import imdb from 'src/assets/images/imdb.png'
-import tmdb from 'src/api/tmdbApi'
 
 import { IoPlayCircleOutline } from "react-icons/io5";
 import { IoMdInformationCircleOutline } from "react-icons/io";
 import { movieDate, tvDate, runTime, rating } from 'src/helper/modifyDatas';
-import { useAxios } from 'src/hooks/useAxios'
+import { getAPIData } from 'src/helper/getAPIData'
 
 const HighlightFilm = ({ filmData }) => {
 
-    const { data, isLoading, error } = useAxios(tmdb, 'get', `/${filmData.media}/${filmData.id}`)
-
-    const { results } = useAxios( tmdb, 'get', `/${filmData.media}/${filmData.id}/videos`)
-    const trailer = results.filter( video => video.type.toLowerCase() === 'trailer')
+    const film = getAPIData(`/${filmData.media}/${filmData.id}`)
+    const videos = getAPIData(`/${filmData.media}/${filmData.id}/videos`).results 
+    const trailer = videos?.filter( video => video.type.toLowerCase() === 'trailer')
 
     const translateMedia = media => {
         if(media === 'movie') {
@@ -31,30 +29,30 @@ const HighlightFilm = ({ filmData }) => {
     return (
     <section className={ styles.HighlightFilm }>
         <div className={ styles.HighlightFilm__dataContainer }>
-            { data && <div className={ styles.HighlightFilm__data }>
+            { film && <div className={ styles.HighlightFilm__data }>
                 <div className={ styles.HighlightFilm__dataFilm }>
                     <div className={ styles.HighlightFilm__date }>
-                        { (data.release_date || data.first_air_date) && <Text className='white' text={ data.release_date? movieDate(data.release_date): tvDate(data.first_air_date, data.last_air_date) } />}
-                        { data.runtime && <Text className='white' text={ runTime(data.runtime) } /> }
+                        { (film.release_date || film.first_air_date) && <Text className='white' text={ film.release_date? movieDate(film.release_date): tvDate(film.first_air_date, film.last_air_date) } />}
+                        { film.runtime && <Text className='white' text={ runTime(film.runtime) } /> }
                     </div>    
                     <Title 
-                        key={data.title? data.title: data.name}
+                        key={film.title? film.title: film.name}
                         animation='tilt-in-left-1'
-                        text={data.title? data.title: data.name} 
+                        text={film.title? film.title: film.name} 
                     />
                     <div className={ styles.HighlightFilm__genres }>
-                        { data.genres?.map( genre => <Text key={ genre.name } className='white' text={ genre.name } /> ) }
+                        { film.genres?.map( genre => <Text key={ genre.name } className='white' text={ genre.name } /> ) }
                     </div>     
                 </div>
-                <Text text={ data?.overview } className='gray' />
-                { data.vote_average && 
+                <Text text={ film?.overview } className='gray' />
+                { film.vote_average && 
                     <div className={ styles.HighlightFilm__rating }>
                         <img src={imdb} alt="Logo do IMDB" />
-                        <Text className='white' text={rating(data.vote_average)} />
+                        <Text className='white' text={rating(film.vote_average)} />
                     </div> }
             </div> }
             <div className={ styles.HighlightFilm__buttons }>
-                { data?.id && <Button 
+                { film?.id && <Button 
                     btnClassName={ styles.HighlightFilm__btnKnowMore } 
                     txtClassName='black' 
                     title='Saiba mais' 
@@ -62,12 +60,12 @@ const HighlightFilm = ({ filmData }) => {
                     href={ `/${translateMedia(filmData.media)}/${filmData.id}` }
                     icon={<IoMdInformationCircleOutline className={ styles.HighlightFilm__iconKnowMore } />} 
                 /> }
-                { trailer[0]?.key && <Button 
+                { trailer?.length > 0 && <Button 
                     btnClassName={ styles.HighlightFilm__btnPlay }
                     txtClassName='white' 
                     title='Assistir ao trailer' 
                     type='button' 
-                    href={ `${trailerBaseURL}${trailer[0].key}?autoplay=1` }
+                    href={ `${trailerBaseURL}${trailer[0]?.key}?autoplay=1` }
                     target='_blank'
                     icon={<IoPlayCircleOutline className={ styles.HighlightFilm__iconPlay } />} 
                 /> }
